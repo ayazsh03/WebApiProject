e24c9342f2034a70c7c82c908c0d2d30d9c32bc7
richard-dsouza <mypass123>
2017-03-21 22:03:28 +0530

    21-Mar-2017

---
+++ LuckyMasale.DAL/DataProviders/Common.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace LuckyMasale.DAL.DataProviders
+{
+    public static class Common
+    {
+        public static string FormatJsonString(string json)
+        {
+            if (String.IsNullOrEmpty(json))
+            {
+                return String.Empty;
+            }
+
+            if (!json.StartsWith("["))
+            {
+                return JObject.Parse(json).ToString();
+            }
+
+            json = "{\"list\":" + json + "}";
+
+            string str = JObject.Parse(json).ToString();
+
+            return str.Substring(13, str.Length - 14).Replace("\n ", "\n");
+        }
+
+        public static string GetRandomInvoiceNumber()
+        {
+            return new Random().Next(999999).ToString();
+        }
+    }
+}
---
+++ LuckyMasale.Shared/DTO/Configuration.cs
@@ -0,0 +1,47 @@
+﻿using PayPal.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuckyMasale.Shared.DTO
+{
+    public static class Configuration
+    {
+        public static readonly string ClientId;
+        public static readonly string ClientSecret;
+
+        static Configuration()
+        {
+            Dictionary<string, string> config = Configuration.GetConfig();
+            Configuration.ClientId = config["clientId"];
+            Configuration.ClientSecret = config["clientSecret"];
+        }
+
+        public static Dictionary<string, string> GetConfig()
+        {
+            return new Dictionary<string, string>()
+            {
+                { "mode", "live" },
+                { "connectionTimeout", "360000" },
+                { "requestRetries", "1" },
+                { "clientId", "Afvi7xg_I0hWPZu3Wyfj2SCt3oPpQyothk5OxTWs0hADf4hNqDj5ImYgno2v_EL_McM_5OzOPAgxFaDO"  },
+                { "clientSecret", "EPIY2-x4b5g3Y4cNWWa6BWKWo1XzBd1yxXzbXegAqVQSHLvC_eeMmu4BsfEJtBg-D1mLdZRw2mhchoSA"  }
+            };
+        }
+
+        private static string GetAccessToken()
+        {           
+            return new OAuthTokenCredential(Configuration.ClientId, Configuration.ClientSecret, Configuration.GetConfig()).GetAccessToken();
+        }
+
+        public static APIContext GetAPIContext(string accessToken)
+        {
+            APIContext apiContext = new APIContext(string.IsNullOrEmpty(accessToken) ? Configuration.GetAccessToken() : accessToken);
+            apiContext.Config = Configuration.GetConfig();
+
+            return apiContext;
+        }
+    }
+}
